2e6992eb2dd916596c24b93a5dfa0ffb0bee37f7
Alex Leith <alexgleith@gmail.com>
2021-05-06 15:06:15 +1000

    Update fix script to only fix where replication didn't work.

--- fix_all.py
+++ fix_all.py
@@ -12,14 +12,21 @@ bucket = s3r.Bucket(SOURCE_BUCKET)
 paths = set(pd.read_csv("missing-data.txt").path)
 
 for path in paths:
+    print(f"Working on {path}")
     resp = s3.list_object_versions(Bucket=SOURCE_BUCKET, Prefix=path)
     del_markers = {item['Key']: item['VersionId'] for item in resp['DeleteMarkers'] if item['IsLatest'] == True}
 
     for key, version in del_markers.items():
-        object = bucket.Object(key)
-        object.delete(VersionId=version)
+        head = s3.head_object(Bucket=SOURCE_BUCKET, Key=key)
 
-        s3r.Object(SOURCE_BUCKET, key).copy_from(
-            CopySource=f'{SOURCE_BUCKET}/{key}',
-            MetadataDirective='REPLACE'
-        )
+        replication_status = head.get("ReplicationStatus")
+        if replication_status and replication_status != "COMPLETED":
+            print(f"Replicating {key}")
+            object = bucket.Object(key)
+            object.delete(VersionId=version)
+            s3r.Object(SOURCE_BUCKET, key).copy_from(
+                CopySource=f'{SOURCE_BUCKET}/{key}',
+                MetadataDirective='REPLACE'
+            )
+        else:
+            print(f"Key is replicated: {key}")
